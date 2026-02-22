a261f425c703002ed2360359c8c84f6362fcc2b3
JuniorAww <50590566+junioraww@users.noreply.github.com>
2026-02-22 17:51:15 +0100

    initial commit

---
+++ src/main/java/me/junioraww/textonhead/Main.java
@@ -0,0 +1,17 @@
+package me.junioraww.textonhead;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+
+
+  }
+
+  @Override
+  public void onDisable() {
+
+  }
+}
